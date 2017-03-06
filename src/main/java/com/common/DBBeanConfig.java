b27806ca2fa48f806da731c32622f3f7d791b53f
zhuzhengping911 <zhuzhengping@zhuzhengpingdeMacBook-Pro.local>
2017-03-07 00:43:47 +0800

    jdbc connect DB

---
+++ src/main/java/com/common/DBBeanConfig.java
@@ -0,0 +1,41 @@
+package com.common;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by zhuzhengping on 2017/3/7.
+ */
+@Configuration
+@EnableTransactionManagement
+@PropertySource(value = {"classpath:config/DBsource.properties"})
+public class DBBeanConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(env.getProperty("source.driverClassName").trim());
+        dataSource.setUrl(env.getProperty("source.url").trim());
+        dataSource.setUsername(env.getProperty("source.username").trim());
+        dataSource.setPassword(env.getProperty("source.password").trim());
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+}
