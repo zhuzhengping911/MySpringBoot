c32faddb795681a13b0cca2aef3f7b199beb4f27
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-03-09 00:04:49 +0800

    many db source

---
+++ src/main/java/com/common/DBBeanConfig_ManyDB.java
@@ -0,0 +1,45 @@
+package com.common;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by zhuzhengping on 2017/3/8.
+ */
+@Configuration
+@EnableTransactionManagement
+public class DBBeanConfig_ManyDB {
+
+    @Bean(name = "oneDataSource")
+    @Qualifier("oneDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.one")
+    public DataSource oneDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "twoDataSource")
+    @Qualifier("twoDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.two")
+    public DataSource twoDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "oneJdbcTemplate")
+    public JdbcTemplate oneJdbcTemplate(@Qualifier("oneDataSource") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "twoJdbcTemplate")
+    public JdbcTemplate twoJdbcTemplate(@Qualifier("twoDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
---
+++ src/test/java/com/example/JdbcManyDBTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by zhuzhengping on 2017/3/8.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class JdbcManyDBTest {
+
+    @Resource(name = "oneJdbcTemplate")
+    protected JdbcTemplate jdbcTemplate1;
+
+    @Resource(name = "twoJdbcTemplate")
+    protected JdbcTemplate jdbcTemplate2;
+
+    @Test
+    public void test(){
+        jdbcTemplate1.update("INSERT INTO tp_author(id,real_name,nick_name) VALUES (?,?,?)",3,"tt","tom");
+
+        jdbcTemplate1.update("INSERT INTO dbgirl.girl(id,age,cupsize) VALUES (?,?,?)",10,"88","tom");
+
+    }
+}
