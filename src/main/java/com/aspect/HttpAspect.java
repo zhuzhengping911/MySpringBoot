770a22e2b33b8cdb2d040d939b847f43bdd43a7c
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-03-19 23:00:51 +0800

    aspect_pointcut

--- src/main/java/com/aspect/HttpAspect.java
+++ src/main/java/com/aspect/HttpAspect.java
@@ -1,8 +1,14 @@
 package com.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by zhuzhengping on 2017/3/19.
@@ -11,8 +17,39 @@ import org.springframework.stereotype.Component;
 @Component
 public class HttpAspect {
 
-    @Before("execution(public * com.controller.*(..))")
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpAspect.class);
+
+    @Pointcut("execution(public * com.controller.*(..))")
     public void log(){
-        System.out.println("this is the aspect before every requset!!!!");
+    }
+
+    @Before("log()")
+    public void before(JoinPoint joinPoint){
+//        System.out.println("this is the aspect before every requset!!!!");
+        LOGGER.info("this is the aspect before every requset!!!!");
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        //url
+        LOGGER.info("url={}",request.getRequestURL());
+        //method
+        LOGGER.info("method={}",request.getMethod());
+        //ip
+        LOGGER.info("id={}",request.getRemoteAddr());
+        //class_method
+        LOGGER.info("class_method={}",joinPoint.getSignature().getDeclaringTypeName() + "," + joinPoint.getSignature().getName());
+        //args[]
+        LOGGER.info("args={}",joinPoint.getArgs());
+    }
+
+    @After("log()")
+    public void after(){
+        LOGGER.info("this is the aspect after every response!!!");
+    }
+
+    @AfterReturning(pointcut = "log()",returning = "object")
+    public void doAfterReturing(Object object){
+        LOGGER.info("response={}",object);
     }
 }
