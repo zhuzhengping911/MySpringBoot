8c5afcd4f984fa271d2bcee26e781b366ce9bcc6
zhuzhengping911 <zhu_zhengping@hotmail.com>
2017-03-23 00:38:45 +0800

    java meal img/html

--- src/test/java/com/example/MailTest.java
+++ src/test/java/com/example/MailTest.java
@@ -4,13 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 import java.util.Properties;
 
 /**
@@ -28,12 +31,55 @@ public class MailTest {
     @Test
     public void send(){
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(new String[]{"1111111111@qq.com"});
-        simpleMailMessage.setFrom("zhu_zhengping@163.com");
+        simpleMailMessage.setTo(new String[]{"zhu_zhengping@hotmail.com"});
+        simpleMailMessage.setFrom("407993723@qq.com");
         simpleMailMessage.setSubject("spring boot mail [text]");
         simpleMailMessage.setText(" this is an simple text ");
 
         mailSender.send(simpleMailMessage);
+
+        System.out.println("mail has been send");
+    }
+    @Test
+    public void sendHtmlMail() throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+        mimeMessageHelper.setTo("zhu_zhengping@hotmail.com");
+        mimeMessageHelper.setFrom("407993723@qq.com");
+        mimeMessageHelper.setSubject("mail test【html】");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<html><head></head>");
+        stringBuilder.append("<body><h1>spring mail test</h1><p>hello ,this is mail test</p></body>");
+        stringBuilder.append("</html>");
+        //启用html
+        mimeMessageHelper.setText(stringBuilder.toString(),true);
+        //发送邮件
+        mailSender.send(mimeMessage);
+
+        System.out.println("mail has been send");
+    }
+
+    @Test
+    public void sendAttachedImageMail() throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
+        mimeMessageHelper.setTo("zhu_zhengping@hotmail.com");
+        mimeMessageHelper.setFrom("407993723@qq.com");
+        mimeMessageHelper.setSubject("mail test【image】");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<html><head></head>");
+        stringBuilder.append("<body><h1>spring mail test</h1><p>hello ,this is mail test</p>");
+        stringBuilder.append("<img src=\"cid:imageId\"/></body>");
+        stringBuilder.append("</html>");
+        //启用html
+        mimeMessageHelper.setText(stringBuilder.toString(),true);
+
+        FileSystemResource img = new FileSystemResource(new File("/Users/zhuzhengping/Downloads/IMG_5067.PNG"));
+        mimeMessageHelper.addInline("imageId",img);
+        //发送邮件
+        mailSender.send(mimeMessage);
 
         System.out.println("mail has been send");
     }
@@ -43,7 +89,7 @@ public class MailTest {
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("zhu_zhengping@hotmail.com","");
+                return new PasswordAuthentication("zhu_zhengping@hotmail.com","password");
 
 
             }
